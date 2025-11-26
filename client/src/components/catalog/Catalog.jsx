55f75d01afe5ed91aaf0b4584da241afbe2a544c
Petia Georgieva <petiaG-va@outlook.com>
2025-11-26 09:47:21 +0200

    add json server
    separate to server and client folders

---
+++ client/src/components/catalog/Catalog.jsx
@@ -0,0 +1,14 @@
+import PlantCard from "../plantCard/PlantCard.jsx";
+
+export default function Catalog() {
+    return (
+        <main>
+            <h1>Catalog</h1>
+
+            <div className="catalog-grid">
+                <PlantCard />
+            </div>
+
+        </main>
+    )
+}
---
+++ client/src/components/plantCard/PlantCard.jsx
@@ -0,0 +1,8 @@
+export default function PlantCard() {
+    return (
+        <div className="plant-card">
+            <img src="https://i.imgur.com/1.jpg" />
+            <h3>Aloe Vera</h3>
+        </div>
+    )
+}
