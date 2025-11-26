099b6c697ed62b735cdc1adac92d6f9f5ebe27cd
Petia Georgieva <petiaG-va@outlook.com>
2025-11-26 10:59:48 +0200

    add dynamic plants in catalog page

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react"
 import PlantCard from "../plantCard/PlantCard.jsx";
 
 export default function Catalog() {
+    const [plants, setPlants] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/plants`)
+            .then(response => response.json())
+            .then(result => setPlants(result))
+            .catch(error => alert(error.message))
+    }, [])
     return (
         <main>
             <h1>Catalog</h1>
 
             <div className="catalog-grid">
-                <PlantCard />
+                {plants.map(plant => <PlantCard key={plant.id} {...plant} />)}
             </div>
 
         </main>
