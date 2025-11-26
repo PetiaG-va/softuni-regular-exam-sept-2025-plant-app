1ef7f61c976c0a68f0fa3cc15cc136e116cd2814
Petia Georgieva <petiaG-va@outlook.com>
2025-11-26 10:59:11 +0200

    create plant card

--- client/src/components/plantCard/PlantCard.jsx
+++ client/src/components/plantCard/PlantCard.jsx
@@ -1,8 +1,26 @@
-export default function PlantCard() {
+import { Link } from "react-router";
+
+export default function PlantCard({
+    id,
+    name,
+    scientific,
+    water,
+    light,
+    abstract,
+    imageUrl,
+
+}) {
     return (
         <div className="plant-card">
-            <img src="https://i.imgur.com/1.jpg" />
-            <h3>Aloe Vera</h3>
+            <img src={imageUrl} alt={name} />
+            <div className="details">
+                <h3 className="name">{name}</h3>
+                <p className="scientific">{scientific}</p>
+                <p className="water">{water}</p>
+                <p className="light">{light}</p>
+                <p className="abstract">{abstract}</p>
+                <Link to={`/catalog/${id}/details`} className="details-btn">Details</Link>
+            </div>
         </div>
     )
 }
